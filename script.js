cff67ecf594032ee0bc20760993fa45ebe2ee096
MD IMRAN HOSSAIN <153748024+imranjewel15@users.noreply.github.com>
2024-10-07 21:59:06 +0600

    Add files via upload
    Enter Method Added

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-const inputBoxField =document.querySelector("#inputBoxField");
-const listContainer =document.querySelector("#listContainer");
-function myFunc(){
-    if (inputBoxField.value === ""){
-        alert("Please type your todo list")
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML= inputBoxField.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML= "&#9986";
-        li.appendChild(span);
-    }
-    inputBoxField.value="";
-    saveData()
+const inputBoxField = document.querySelector("#inputBoxField");
+const listContainer = document.querySelector("#listContainer");
+function myFunc() {
+  if (inputBoxField.value === "") {
+    alert("Please type your todo list");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBoxField.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "&#9986";
+    li.appendChild(span);
+  }
+  inputBoxField.value = "";
+  saveData();
 }
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData()
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData()
-    }
-},false)
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+inputBoxField.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
-function getTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
+function getTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 getTask();
